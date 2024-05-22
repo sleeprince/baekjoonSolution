1288c85eb863aebfa1b837b92aaa3c06fd4c9547
sleeprince <wowkwangsu@gmail.com>
2024-05-22 17:30:01 +0900

    adding array solution

---
+++ src/D_1dim_array/Main_1546.java
@@ -0,0 +1,35 @@
+package D_1dim_array;
+// 세준이는 기말고사를 망쳤다. 세준이는 점수를 조작해서 집에 가져가기로 했다. 일단 세준이는 자기 점수 중에 최댓값을 골랐다. 이 값을 M이라고 한다. 그리고 나서 모든 점수를 점수/M*100으로 고쳤다.
+// 예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 수학점수는 50/70*100이 되어 71.43점이 된다.
+// 세준이의 성적을 위의 방법대로 새로 계산했을 때, 새로운 평균을 구하는 프로그램을 작성하시오.
+
+// 첫째 줄에 시험 본 과목의 개수 N이 주어진다. 이 값은 1000보다 작거나 같다. 둘째 줄에 세준이의 현재 성적이 주어진다. 이 값은 100보다 작거나 같은 음이 아닌 정수이고, 적어도 하나의 값은 0보다 크다.
+
+// 첫째 줄에 새로운 평균을 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-2 이하이면 정답이다.
+public class Main_1546{
+    private static int input() throws Exception{
+        int a = 0;
+        int result = 0;
+        while(true){
+            a = System.in.read();
+            //if(a == 10) continue;
+            if(a < '0' || a > '9') return result;
+            result *= 10;
+            result += a - '0';
+        }
+    }
+    public static void main(String[] args) throws Exception{
+        double[] score = new double[1000];
+        int cnt = input();
+        double max = 0;
+        double sum = 0;
+        for(int i = 0; i < cnt; i++){
+            score[i] = (double)input();
+            if(max < score[i]) max = score[i];            
+        }
+        for(int j = 0; j < cnt; j++){
+            sum += score[j]/max*100;
+        }
+        System.out.print(sum/cnt);
+    }
+}
---
+++ src/D_1dim_array/Main_3052.java
@@ -0,0 +1,35 @@
+package D_1dim_array;
+// 두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다. 
+// 수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
+
+// 첫째 줄부터 열번째 줄 까지 숫자가 한 줄에 하나씩 주어진다. 이 숫자는 1,000보다 작거나 같고, 음이 아닌 정수이다.
+
+// 첫째 줄에, 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력한다.
+public class Main_3052 {
+    private static int input() throws Exception{
+        int a = 0;
+        int result = 0;
+        while(true){
+            a = System.in.read();
+            if(a == 10) continue;
+            if(a < '0' || a > '9') return result;
+            result *= 10;
+            result += a - '0';
+        }
+    }
+    public static void main(String[] args) throws Exception{
+        int[] arr = new int[42];
+        int tmp;
+        int cnt = 0;
+        for(int i = 0; i < 10; i++){
+            tmp = input()%42;
+            arr[tmp]++;
+        }
+        for(int i = 0; i < 42; i++){
+            if(arr[i] != 0) {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
